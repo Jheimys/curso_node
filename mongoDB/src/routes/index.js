9275d83378033ae756a9b368e0aaa761f4c822e7
Jheimys Bassani <jheimysbb@gmail.com>
2022-04-12 07:47:04 -0300

    subindo arquivos do mongo db

--- mongoDB/src/routes/index.js
+++ mongoDB/src/routes/index.js
@@ -4,21 +4,26 @@
 const router = require('express').Router()
 
 const CustomersController = require('../controllers/customers')
+const IndexController = require('../controllers/index')
 
 //Rotas
-router.get('/', (req, res) =>{
-    res.render('index',{
-        title:'Titulo teste'
-    })
-})
+router.get('/', IndexController.index)
 
-router.get('/register', (req, res) =>{
-    res.render('register',{
-        title:'Cadastro de clientes'
-    })
-})
 
+//regitro
+router.get('/register', CustomersController.index)
 router.post('/register/add', CustomersController.add)
+
+//listar
+router.get('/list', CustomersController.list)
+
+//editar
+router.get('/edit', CustomersController.formEdit)
+router.post('/edit/:id', CustomersController.edit)
+
+//remove
+
+router.get('/remove/:id', CustomersController.remove)
 
 
 
